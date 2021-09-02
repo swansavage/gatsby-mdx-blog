a65144d44069354e37c5e21c4eb25d5e7b41059a
Shaun Savage <swansavage@gmail.com>
2021-09-02 11:09:24 -0500

    newsletter setup / css fix in hero

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,40 @@ import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your indobx</h2>
+          <h4>I write to my friends every few weeks</h4>
+          <form
+            className="contact-form"
+            name="testing-contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="testing-contact" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <button type="submit" className="btn submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
