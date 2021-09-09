50104b7799e6c90b7452ec47ede6ca2235990a77
Shaun Savage <swansavage@gmail.com>
2021-09-09 11:56:04 -0500

    posts setup on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,40 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({data}) => {
+  const {allMdx:{nodes:posts}} = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
+
+
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        id
+        frontmatter {
+          author
+          category
+          date(formatString: "MMMM, Do YYYY")
+          readTime
+          slug
+          title
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default IndexPage
