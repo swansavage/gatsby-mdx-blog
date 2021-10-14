bd40b66a99d44b84b1b986cd67bdb02fb5da7d21
swansavage@github.com <swansavage@gmail.com>
2021-10-14 13:37:37 -0500

    added code block components

---
+++ root-mdx.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+import { Headings, Code, Blockquote, Example } from './src/components/Complete'
+
+const Testing = ({ children }) => {
+  return <code className="code">{children}</code>
+}
+
+const components = {
+  h2: Headings.myH2,
+  h4: Headings.myH4,
+  inlineCode: Code,
+  blockquote: Blockquote,
+  pre: Example,
+}
+
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
